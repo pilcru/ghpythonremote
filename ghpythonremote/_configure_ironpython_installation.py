414cf840289486f22f61384bb19fd398151ddffc
Pierre Cuvilliers <pierre.cuvilliers@gmail.com>
2017-09-04 13:38:30 -0400

    Quote windows path

--- ghpythonremote/_configure_ironpython_installation.py
+++ ghpythonremote/_configure_ironpython_installation.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
         location = sys.argv[1]
     rhino_ironpython_path = get_rhino_ironpython_path(location=location)
     package_name = __package__.split('.')[0]
-    pip_cmd = ['install', package_name, '--target=' + rhino_ironpython_path,
+    pip_cmd = ['install', package_name, '--target="' + rhino_ironpython_path + '"',
                '--upgrade', '--no-binary :all:', '--no-compile', '--ignore-requires-python']
     print('\n\nThis will install ghpythonremote in Rhino IronPython with the command:')
     print('pip ' + ' '.join(pip_cmd))
