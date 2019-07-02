49dccc71f344ea8d7d3923a2e32de25cf530dc94
Pierre Cuvilliers <pierre.cuvilliers@gmail.com>
2019-07-01 18:54:52 -0700

    Update to RPyC 4.1.0

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ setup(
     ],
 
     install_requires=[
-        'rpyc>=3.4.6'
+        'rpyc>=4.1.0'
     ],
     dependency_links=[
         'https://github.com/pilcru/rpyc/archive/v3.4.6.tar.gz#egg=rpyc-3.4.6',
